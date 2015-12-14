e4220989f2f6941b50b951d2999c6404ac362a5d
smakman <jurjen@diffuse.nl>
2015-12-14 16:51:45 +0100

    update gulpfile

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -6,12 +6,10 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
-    coffee = require('gulp-coffee'),
+    del = require('del'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
@@ -27,39 +25,36 @@ var gulp = require('gulp'),
     
 // Define paths
 var paths = {  
-  scripts:   ['src/js/*.coffee', 'src/js/*.js'],
-  styles:    ['src/css/*.scss', 'src/css/*.sass', 'src/css/*.css'],
-  images:    ['src/images/*.png'],
-  templates: ['src/templates/*.ejs']
+  scripts:   ['./src/js/*.js'],
+  styles:    ['./src/css/*.{scss,sass,css}'],
+  images:    ['src/images/**', '!src/images/sprite/**', '!src/images/sprite/'],
+  templates: ['./src/templates/*.ejs']
 };
 
 // CSS
 gulp.task('css', function() {
-  return gulp.src(paths.styles)
-    .pipe(sass({ 
-      style: 'expanded',
-      loadPath: [
-        process.cwd() + '/src/css/partials',
-        process.cwd() + '/src/vendor'
-      ]
-    }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('dist/assets/css'))
-    .pipe(notify({ message: 'CSS task complete' }));
+  return sass(paths.styles, {
+    style: 'expanded',
+    loadPath: [
+      process.cwd() + '/src/css/partials',
+      process.cwd() + '/src/vendor'
+    ]
+  })
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+  .pipe(gulp.dest('dist/assets/css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(minifycss())
+  .pipe(gulp.dest('dist/assets/css'))
+  .pipe(notify({ message: 'CSS task complete' }));
 });
 
 // Javascript
 gulp.task('js', function() {
   return gulp.src(paths.scripts)
-    .pipe(include())
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
+    .pipe(include().on('error', gutil.log))
     .pipe(gulp.dest('dist/assets/js'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
+    .pipe(uglify().on('error', gutil.log))
     .pipe(gulp.dest('dist/assets/js'))
     .pipe(notify({ message: 'JS task complete' }));
 });
@@ -68,7 +63,7 @@ gulp.task('js', function() {
 gulp.task('sprites', function () {
   return gulp.src('./src/images/sprite/*.png')
     .pipe(sprite({
-      name: 'sprite.png',
+      name: 'sprite',
       style: '_sprite.sass',
       cssPath: '../images',
       processor: 'sass',
@@ -83,10 +78,6 @@ gulp.task('images', function() {
   return gulp.src(paths.images)
     .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/assets/images'))
-    //.pipe(rev())
-    //.pipe(gulp.dest('dist/assets/images'))
-    //.pipe(rev.manifest())
-    //.pipe(gulp.dest('.'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
@@ -100,8 +91,7 @@ gulp.task('templates', function() {
 
 // Clean up
 gulp.task('clean', function() {
-  return gulp.src(['dist/assets/css', 'dist/assets/js', 'dist/assets/images', 'dist/assets/fonts', 'dist/*.html'], {read: false})
-    .pipe(clean());
+  return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/images', 'dist/assets/fonts', 'dist/*.html']);
 });
 
 // Rev all files
@@ -128,8 +118,8 @@ gulp.task('connect', function() {
   var connect = require('connect');
   var app = connect()
       .use(require('connect-livereload')({ port: 35729 }))
-      .use(connect.static('dist'))
-      .use(connect.directory('dist'));
+      .use(require('serve-static')('dist'))
+      .use(require('serve-index')('dist'));
         
   require('http').createServer(app)
     .listen(9000)
@@ -165,11 +155,11 @@ gulp.task('watch', ['connect', 'serve'], function() {
   gulp.watch('src/**/*.{eot,svg,ttf.woff}', ['fonts']);
   
   // Create LiveReload server
-  var server = livereload();
+  livereload({ start: true });
 
   // Watch any files in assets folder reload on change
   gulp.watch(['dist/assets/**', 'dist/*.html']).on('change', function(file) {
-    server.changed(file.path);
+    livereload.changed(file.path);
   });
 
 });
